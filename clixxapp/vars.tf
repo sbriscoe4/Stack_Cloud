2c1f7093a1b6dd1825aaf0bc0a81fd24b45d4f36
sbriscoe4 <sbriscoe4@gmail.com>
2021-04-26 21:43:57 -0400

    initial commit

---
+++ clixxapp/vars.tf
@@ -0,0 +1,58 @@
+variable "AWS_ACCESS_KEY" {}
+
+variable "AWS_SECRET_KEY" {}
+
+
+variable "AWS_REGION" {
+  default = "us-east-1"  
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "mykey"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "mykey.pub"
+}
+
+variable "AMIS" {
+  type = map(string)
+  default = {
+    #us-east-1 = "ami-13be557e"
+    us-east-1 = "ami-08f3d892de259504d"
+    us-west-2 = "ami-06b94666"
+    eu-west-1 = "ami-844e0bf7"
+  }
+}
+
+#variable "RDS_PASSWORD" {}
+
+#variable "INSTANCE_USERNAME" {}
+
+#variable "DB_NAME" {}
+
+#variable "DB_USER" {}
+
+#variable "DB_PASSWORD" {}
+
+#variable "DB_HOST" {}
+
+variable "SUBNET_ID" {
+    default = "subnet-10e47f4f"
+}
+
+#variable "RDS_ENDPOINT" {}
+
+variable "MOUNT_POINT"{
+    default = "/var/www/html"
+}
+
+variable "A_ZONE"{
+    type = map(string)
+    default = {
+      one = "us-east-1a"
+      two = "us-east-1c" 
+      three = "us-east-1c"
+      four = "us-east-1d"    
+    }
+}
